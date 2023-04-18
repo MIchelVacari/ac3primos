6b34b18d6f1c43accc209678766b8987ea81bf05
MichelVacariADS <michelvcruz44@gmail.com>
2023-04-18 09:53:19 -0300

    first commit

---
+++ primos.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+app.route('/')
+
+def primos():
+    qtdTotal = 100
+    primos = "1,2"
+    candPrimo = 3 
+    qtdEncontrada = 2
+    
+    while qtdEncontrada < qtdTotal:
+        for i in range (2, candPrimo):
+            if candPrimo % i == 0:
+                ehPrimo == 0
+                break
+        if ehPrimo == 1:
+            primos = primos + str(candPrimo) + ","
+            qtdEncontrada +=1
+        ehPrimo = 1
+    candPrimo += 1
+    return primos
+    
+    
+    
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
